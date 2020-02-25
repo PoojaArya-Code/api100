419d8999c44731f3a77683f05dbd66483e003daf
Pooja <61430577+PoojaArya-Code@users.noreply.github.com>
2020-02-25 20:23:48 +0000

    Almost done for tuesday

---
+++ LibraryApi/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using LibraryApi.Controllers.Domain;
+using LibraryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class BooksController : Controller
+    {
+        LibraryDataContext Context;
+
+        public BooksController(LibraryDataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("books")]
+
+        public IActionResult GetAllBooks()
+        {
+            var response = new GetBooksResponse();
+            response.Data = Context.Books
+                .Where(b=> b.InInventory)
+                .Select(b => new BookSummaryItem { Id = b.Id, Title = b.Title, Author = b.Author })
+                .ToList();
+            //var response = Context.Books.ToList();
+            return Ok(response);
+        }
+    }
+}
